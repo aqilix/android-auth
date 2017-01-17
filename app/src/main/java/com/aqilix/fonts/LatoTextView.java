89dceb06ef25766a088037a61eb93984823d03e4
Dolly Aswin <dolly.aswin@gmail.com>
2017-01-17 15:30:35 +0700

    Add Lato Fonts

---
+++ app/src/main/java/com/aqilix/fonts/LatoTextView.java
@@ -0,0 +1,32 @@
+package com.aqilix.fonts;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class LatoTextView extends TextView {
+
+    public LatoTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public LatoTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public LatoTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/Lato-Light.ttf");
+            setTypeface(tf);
+        }
+    }
+
+}
